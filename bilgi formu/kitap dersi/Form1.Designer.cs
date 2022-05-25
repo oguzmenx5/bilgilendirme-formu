7b972e17a87160f8962e85e4f652cdfe8741e311
oguzmenx5 <oguzhanozd55@gmail.com>
2022-05-25 14:57:59 +0300

    form uyhgulası ile
    c# ile bilgilendirme formu

---
+++ bilgi formu/kitap dersi/Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace kitap_dersi
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_tıklayın = new System.Windows.Forms.Button();
+            this.tb_araba = new System.Windows.Forms.TextBox();
+            this.tb_eposta = new System.Windows.Forms.TextBox();
+            this.tb_meslek = new System.Windows.Forms.TextBox();
+            this.tb_yaş = new System.Windows.Forms.TextBox();
+            this.tb_soyad = new System.Windows.Forms.TextBox();
+            this.tb_ad = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lb_ad = new System.Windows.Forms.Label();
+            this.lb_soyad = new System.Windows.Forms.Label();
+            this.lb_yaş = new System.Windows.Forms.Label();
+            this.lb_meslek = new System.Windows.Forms.Label();
+            this.lb_posta = new System.Windows.Forms.Label();
+            this.lb_araba = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_tıklayın
+            // 
+            this.btn_tıklayın.Location = new System.Drawing.Point(270, 417);
+            this.btn_tıklayın.Name = "btn_tıklayın";
+            this.btn_tıklayın.Size = new System.Drawing.Size(124, 54);
+            this.btn_tıklayın.TabIndex = 0;
+            this.btn_tıklayın.Text = "tıklayın";
+            this.btn_tıklayın.UseVisualStyleBackColor = true;
+            this.btn_tıklayın.Click += new System.EventHandler(this.button1_Click);
+            this.btn_tıklayın.Enter += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_araba
+            // 
+            this.tb_araba.Location = new System.Drawing.Point(139, 283);
+            this.tb_araba.Name = "tb_araba";
+            this.tb_araba.Size = new System.Drawing.Size(129, 27);
+            this.tb_araba.TabIndex = 1;
+            // 
+            // tb_eposta
+            // 
+            this.tb_eposta.Location = new System.Drawing.Point(139, 231);
+            this.tb_eposta.Name = "tb_eposta";
+            this.tb_eposta.Size = new System.Drawing.Size(129, 27);
+            this.tb_eposta.TabIndex = 3;
+            // 
+            // tb_meslek
+            // 
+            this.tb_meslek.Location = new System.Drawing.Point(139, 178);
+            this.tb_meslek.Name = "tb_meslek";
+            this.tb_meslek.Size = new System.Drawing.Size(129, 27);
+            this.tb_meslek.TabIndex = 4;
+            // 
+            // tb_yaş
+            // 
+            this.tb_yaş.Location = new System.Drawing.Point(139, 125);
+            this.tb_yaş.Name = "tb_yaş";
+            this.tb_yaş.Size = new System.Drawing.Size(129, 27);
+            this.tb_yaş.TabIndex = 5;
+            // 
+            // tb_soyad
+            // 
+            this.tb_soyad.Location = new System.Drawing.Point(139, 77);
+            this.tb_soyad.Name = "tb_soyad";
+            this.tb_soyad.Size = new System.Drawing.Size(129, 27);
+            this.tb_soyad.TabIndex = 6;
+            // 
+            // tb_ad
+            // 
+            this.tb_ad.Location = new System.Drawing.Point(139, 24);
+            this.tb_ad.Name = "tb_ad";
+            this.tb_ad.Size = new System.Drawing.Size(129, 27);
+            this.tb_ad.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 283);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "araba modeli";
+          //  this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "e-posta";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 179);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "meslek";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(26, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 20);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "yaş";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(26, 73);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(48, 20);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "soyad";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(26, 31);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(26, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "ad";
+            // 
+            // lb_ad
+            // 
+            this.lb_ad.AutoSize = true;
+            this.lb_ad.Location = new System.Drawing.Point(473, 31);
+            this.lb_ad.Name = "lb_ad";
+            this.lb_ad.Size = new System.Drawing.Size(49, 20);
+            this.lb_ad.TabIndex = 14;
+            this.lb_ad.Text = "adınız";
+            // 
+            // lb_soyad
+            // 
+            this.lb_soyad.AutoSize = true;
+            this.lb_soyad.Location = new System.Drawing.Point(473, 84);
+            this.lb_soyad.Name = "lb_soyad";
+            this.lb_soyad.Size = new System.Drawing.Size(71, 20);
+            this.lb_soyad.TabIndex = 15;
+            this.lb_soyad.Text = "soyadınız";
+            // 
+            // lb_yaş
+            // 
+            this.lb_yaş.AutoSize = true;
+            this.lb_yaş.Location = new System.Drawing.Point(473, 137);
+            this.lb_yaş.Name = "lb_yaş";
+            this.lb_yaş.Size = new System.Drawing.Size(53, 20);
+            this.lb_yaş.TabIndex = 16;
+            this.lb_yaş.Text = "yaşınız";
+            // 
+            // lb_meslek
+            // 
+            this.lb_meslek.AutoSize = true;
+            this.lb_meslek.Location = new System.Drawing.Point(473, 186);
+            this.lb_meslek.Name = "lb_meslek";
+            this.lb_meslek.Size = new System.Drawing.Size(80, 20);
+            this.lb_meslek.TabIndex = 17;
+            this.lb_meslek.Text = "mesleğiniz";
+            // 
+            // lb_posta
+            // 
+            this.lb_posta.AutoSize = true;
+            this.lb_posta.Location = new System.Drawing.Point(473, 231);
+            this.lb_posta.Name = "lb_posta";
+            this.lb_posta.Size = new System.Drawing.Size(79, 20);
+            this.lb_posta.TabIndex = 18;
+            this.lb_posta.Text = "e-postanız";
+            // 
+            // lb_araba
+            // 
+            this.lb_araba.AutoSize = true;
+            this.lb_araba.Location = new System.Drawing.Point(473, 283);
+            this.lb_araba.Name = "lb_araba";
+            this.lb_araba.Size = new System.Drawing.Size(121, 20);
+            this.lb_araba.TabIndex = 19;
+            this.lb_araba.Text = "arbanızın modeli";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(676, 532);
+            this.Controls.Add(this.lb_araba);
+            this.Controls.Add(this.lb_posta);
+            this.Controls.Add(this.lb_meslek);
+            this.Controls.Add(this.lb_yaş);
+            this.Controls.Add(this.lb_soyad);
+            this.Controls.Add(this.lb_ad);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_ad);
+            this.Controls.Add(this.tb_soyad);
+            this.Controls.Add(this.tb_yaş);
+            this.Controls.Add(this.tb_meslek);
+            this.Controls.Add(this.tb_eposta);
+            this.Controls.Add(this.tb_araba);
+            this.Controls.Add(this.btn_tıklayın);
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+       //     this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_tıklayın;
+        private System.Windows.Forms.TextBox tb_araba;
+        private System.Windows.Forms.TextBox tb_eposta;
+        private System.Windows.Forms.TextBox tb_meslek;
+        private System.Windows.Forms.TextBox tb_yaş;
+        private System.Windows.Forms.TextBox tb_soyad;
+        private System.Windows.Forms.TextBox tb_ad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lb_ad;
+        private System.Windows.Forms.Label lb_soyad;
+        private System.Windows.Forms.Label lb_yaş;
+        private System.Windows.Forms.Label lb_meslek;
+        private System.Windows.Forms.Label lb_posta;
+        private System.Windows.Forms.Label lb_araba;
+    }
+}
+
---
+++ bilgi formu/kitap dersi/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kitap_dersi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            lb_ad.Text = tb_ad.Text;
+            lb_soyad.Text = tb_soyad.Text;
+            lb_yaş.Text = tb_yaş.Text;
+            lb_meslek.Text = tb_meslek.Text;
+            lb_posta.Text = tb_eposta.Text;
+            lb_araba.Text = tb_araba.Text;
+
+
+
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode==Keys.Enter)
+
+            {
+                btn_tıklayın.PerformClick();
+            }
+        }
+    }
+}
